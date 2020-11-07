be9b0a8eb9a3cf11a2345abb9c94a65080f17d0d
WiRight <ruslan.mahin1@gmail.com>
2020-11-07 12:11:56 +0300

    Add -version CLI argument

--- main.go
+++ main.go
@@ -1,246 +1,246 @@
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-	"os/exec"
-	"strings"
+  "bufio"
+  "flag"
+  "fmt"
+  "io/ioutil"
+  "log"
+  "os"
+  "os/exec"
+  "strings"
 
-	_ "github.com/joho/godotenv/autoload"
-	"gopkg.in/yaml.v2"
+  _ "github.com/joho/godotenv/autoload"
+  "gopkg.in/yaml.v2"
 )
 
 const (
-	input   = "input" // Read info from user input
-	env     = "env"   // Get param from env
-	version = "1.2.2" // Application version
+  input   = "input" // Read info from user input
+  env     = "env"   // Get param from env
+  version = "1.2.2" // Application version
 )
 
 var (
-	debugMode = false // Debug mode - print debug info
-	argSym    = "$"   // Start of argument TODO: maybe set in config
+  debugMode = false // Debug mode - print debug info
+  argSym    = "$"   // Start of argument TODO: maybe set in config
 )
 
 // Command structure
 type iCommand struct {
-	Name     string            `yaml:"name"`
-	Args     map[string]string `yaml:"args"`
-	Commands []string          `yaml:"commands"`
+  Name     string            `yaml:"name"`
+  Args     map[string]string `yaml:"args"`
+  Commands []string          `yaml:"commands"`
 }
 
 // Struct of dron.yaml file
 type config struct {
-	Commands []iCommand `yaml:"commands"`
+  Commands []iCommand `yaml:"commands"`
 }
 
 // Print debug into
 func debug(data ...interface{}) {
-	if debugMode == true {
-		fmt.Println("[debug]", data)
-	}
+  if debugMode == true {
+    fmt.Println("[debug]", data)
+  }
 }
 
 // Check that command has arguments
 func checkHasArgPointer(command string) int {
-	for i, v := range command {
-		if (string(v)) == argSym {
-			return i
-		}
-	}
+  for i, v := range command {
+    if (string(v)) == argSym {
+      return i
+    }
+  }
 
-	return -1
+  return -1
 }
 
 // Parsing and processing ENV argument
 func parseEnv(arg string) string {
-	// $env(ENV_NAME)
-	envName := arg[5 : len(arg)-1]
+  // $env(ENV_NAME)
+  envName := arg[5 : len(arg)-1]
 
-	envVal, exist := os.LookupEnv(envName)
+  envVal, exist := os.LookupEnv(envName)
 
-	if exist == false {
-		log.Fatal(fmt.Sprintf("Env param '%s' not exist!", envName))
-	}
+  if exist == false {
+    log.Fatal(fmt.Sprintf("Env param '%s' not exist!", envName))
+  }
 
-	return envVal
+  return envVal
 }
 
 // Parsing and processing INPUT argument
 func parseInput(argName string) string {
-	scanner := bufio.NewScanner(os.Stdin)
+  scanner := bufio.NewScanner(os.Stdin)
 
-	fmt.Printf("[%s]> ", argName)
+  fmt.Printf("[%s]> ", argName)
 
-	scanner.Scan()
+  scanner.Scan()
 
-	return scanner.Text()
+  return scanner.Text()
 }
 
 // Parse arguments
 func parseArgs(args map[string]string, command string) string {
-	result := command
+  result := command
 
-	for argPos := checkHasArgPointer(result); argPos > -1; argPos = checkHasArgPointer(result) {
-		length := len(result)
+  for argPos := checkHasArgPointer(result); argPos > -1; argPos = checkHasArgPointer(result) {
+    length := len(result)
 
-		argName := ""
-		pos := -1
+    argName := ""
+    pos := -1
 
-		for j := argPos + 1; j < length; j++ {
-			ch := string(result[j])
-			argName += ch
+    for j := argPos + 1; j < length; j++ {
+      ch := string(result[j])
+      argName += ch
 
-			if strings.ContainsAny(ch, "\n\t\"' ") || j == length-1 {
-				pos = j
+      if strings.ContainsAny(ch, "\n\t\"' ") || j == length-1 {
+        pos = j
 
-				if strings.ContainsAny(ch, `"'(){}[]`) {
-					pos = j - 1
-				}
+        if strings.ContainsAny(ch, `"'(){}[]`) {
+          pos = j - 1
+        }
 
-				break
-			}
-		}
+        break
+      }
+    }
 
-		argName = strings.Trim(argName, "\n\t'\" ")
+    argName = strings.Trim(argName, "\n\t'\" ")
 
-		if argName != "" && args[argName] != "" && pos != -1 {
-			argParam := args[argName]
+    if argName != "" && args[argName] != "" && pos != -1 {
+      argParam := args[argName]
 
-			debug("ARG_NAME", argName, argParam)
+      debug("ARG_NAME", argName, argParam)
 
-			end := ""
+      end := ""
 
-			if pos == length-1 {
-				end = ""
-			} else {
-				end = result[pos:]
-			}
+      if pos == length-1 {
+        end = ""
+      } else {
+        end = result[pos:]
+      }
 
-			// CHECK ENV ---
+      // CHECK ENV ---
 
-			if strings.Contains(argParam, env) {
-				argParam = parseEnv(argParam)
-			}
+      if strings.Contains(argParam, env) {
+        argParam = parseEnv(argParam)
+      }
 
-			// --- CHECK ENV
+      // --- CHECK ENV
 
-			// CHECK INPUT ---
+      // CHECK INPUT ---
 
-			if strings.Contains(argParam, input) {
-				argParam = parseInput(argName)
-			}
+      if strings.Contains(argParam, input) {
+        argParam = parseInput(argName)
+      }
 
-			// --- CHECK INPUT
+      // --- CHECK INPUT
 
-			result = fmt.Sprintf("%s%s%s", result[:argPos], argParam, end)
-		} else {
-			debug("[error]", argName, args[argName], pos)
+      result = fmt.Sprintf("%s%s%s", result[:argPos], argParam, end)
+    } else {
+      debug("[error]", argName, args[argName], pos)
 
-			log.Fatal(fmt.Sprintf("Argument $%s not found in `args` block", argName))
-		}
-	}
+      log.Fatal(fmt.Sprintf("Argument $%s not found in `args` block", argName))
+    }
+  }
 
-	return result
+  return result
 }
 
 func main() {
-	// CLI arguments
-	isDebug := flag.Bool("debug", false, "Print debug info")
-	showList := flag.Bool("list", false, "Print list commands")
-	showVersion := flag.Bool("version", false, "Show version")
+  // CLI arguments
+  isDebug := flag.Bool("debug", false, "Print debug info")
+  showList := flag.Bool("list", false, "Print list commands")
+  showVersion := flag.Bool("version", false, "Show version")
 
-	flag.Parse()
+  flag.Parse()
 
-	debugMode = *isDebug
-	// -------------------------
+  debugMode = *isDebug
+  // -------------------------
 
-	c := config{}
+  c := config{}
 
-	data, err := ioutil.ReadFile("./dron.yaml")
+  data, err := ioutil.ReadFile("./dron.yaml")
 
-	if err != nil {
-		data2, err2 := ioutil.ReadFile("./dron.yml")
+  if err != nil {
+    data2, err2 := ioutil.ReadFile("./dron.yml")
 
-		if err2 != nil {
-			log.Fatal("File 'dron.yaml' or 'dron.yml' can not be located in current folder")
-		}
+    if err2 != nil {
+      log.Fatal("File 'dron.yaml' or 'dron.yml' can not be located in current folder")
+    }
 
-		data = data2
-	}
+    data = data2
+  }
 
-	if err := yaml.Unmarshal(data, &c); err != nil {
-		log.Fatal("Cant read config!\n", err)
-	}
+  if err := yaml.Unmarshal(data, &c); err != nil {
+    log.Fatal("Cant read config!\n", err)
+  }
 
-	// Check that using -list argument
-	if *showList == true {
-		for _, v := range c.Commands {
-			fmt.Println(v.Name)
-		}
+  // Check that using -list argument
+  if *showList == true {
+    for _, v := range c.Commands {
+      fmt.Println(v.Name)
+    }
 
-		return
-	}
+    return
+  }
 
-	// Check that using -version argument
-	if *showVersion == true {
-		fmt.Println("Dron version:", version)
+  // Check that using -version argument
+  if *showVersion == true {
+    fmt.Println("Dron version:", version)
 
-		return
-	}
+    return
+  }
 
-	// Check the first argument exist
-	var fArg string
+  // Check the first argument exist
+  var fArg string
 
-	for _, v := range os.Args[1:] {
-		// Собираем только аргументы без "-"
-		if !strings.Contains(v, "-") {
-			fArg = v
-		}
-	}
+  for _, v := range os.Args[1:] {
+    // Собираем только аргументы без "-"
+    if !strings.Contains(v, "-") {
+      fArg = v
+    }
+  }
 
-	if fArg == "" {
-		log.Fatal("Command name not specified")
-	}
+  if fArg == "" {
+    log.Fatal("Command name not specified")
+  }
 
-	commandExist := false
-	var command iCommand
+  commandExist := false
+  var command iCommand
 
-	for _, k := range c.Commands {
-		if k.Name == fArg {
-			commandExist = true
-			command = k
+  for _, k := range c.Commands {
+    if k.Name == fArg {
+      commandExist = true
+      command = k
 
-			break
-		}
-	}
+      break
+    }
+  }
 
-	if commandExist == false {
-		log.Fatal("Unknown command \"", fArg, "\"")
-	}
+  if commandExist == false {
+    log.Fatal("Unknown command \"", fArg, "\"")
+  }
 
-	for i, k := range command.Commands {
-		debug("Run [", i, "] command")
-		debug("-->", k)
+  for i, k := range command.Commands {
+    debug("Run [", i, "] command")
+    debug("-->", k)
 
-		parsed := parseArgs(command.Args, k)
+    parsed := parseArgs(command.Args, k)
 
-		debug("[parsed]", parsed)
+    debug("[parsed]", parsed)
 
-		cmd := exec.Command("bash", "-c", parsed)
-		cmd.Stdout = os.Stdout
+    cmd := exec.Command("bash", "-c", parsed)
+    cmd.Stdout = os.Stdout
 
-		if err := cmd.Run(); err != nil {
-			log.Fatal("Cant run command!\n", err)
-		}
+    if err := cmd.Run(); err != nil {
+      log.Fatal("Cant run command!\n", err)
+    }
 
-		//if err := cmd.Wait(); err != nil {
-		//	log.Fatal("Cant Wait end of command!", err)
-		//}
-	}
+    //if err := cmd.Wait(); err != nil {
+    //	log.Fatal("Cant Wait end of command!", err)
+    //}
+  }
 
-	println(">>> Done!")
+  println(">>> Done!")
 }
